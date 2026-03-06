b40b5bb70884d87690be616f5eaf3629c57dd752
Julio Santa Cruz <bartacruz@gmail.com>
2026-03-06 14:34:50 -0300

    Kanban sidebar sizing and layout

--- static/src/components/kanban_controller.js
+++ static/src/components/kanban_controller.js
@@ -4,6 +4,7 @@ import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { DriverList } from "./driver_list";
 import { registry } from "@web/core/registry";
 import { kanbanView } from "@web/views/kanban/kanban_view";
+import { onRendered, onMounted } from "@odoo/owl";
 
 export class TruckingKanbanController extends KanbanController {
     static components = { ...KanbanController.components, DriverList}
@@ -12,11 +13,29 @@ export class TruckingKanbanController extends KanbanController {
     setup() {
         super.setup();
         this.searchKey = Symbol("isFromTruckingKanban");
+        // this.kanban = $(".o_kanban_renderer");
+        // console.debug("kanban on setup",this.kanban);
+        // onMounted(async () => {
+        //     console.debug("kanban before mounted",this.kanban);
+        //     this.kanban = $(".o_kanban_renderer");
+        //     this.kanban.css('margin-left','250px');
+        //     console.debug("kanban after mounted",this.kanban);
+        // });
     }
     toggleFold() {
-        
-        $(this.rootRef.el).find(".o_trucking_kanban_sidebar").toggleClass("folded");
+        const el = $(this.rootRef.el).find(".o_trucking_kanban_sidebar");
+        el.toggleClass("folded");
+        // var kanban = $(".o_kanban_renderer");
+        // console.debug("kanban on toggle",this.kanban);
+        // console.debug("kanban private on toggle",kanban);
+        // if (el.hasClass("folded")) {
+        //     kanban.css('margin-left','60px');
+        // } else {
+        //     kanban.css('margin-left','250px');
+        // }
+
     }
+    
     selectDriver(partner_id, partner_name) {
         const driverFilters = this.env.searchModel.getSearchItems((searchItem) =>
             searchItem[this.searchKey]
