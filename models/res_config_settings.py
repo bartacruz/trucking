65d136065c800b608a4bf8cc6bdf803af27fc662
Julio Santa Cruz <bartacruz@gmail.com>
2026-03-22 00:37:01 -0300

    [FIX] res.config parameters.

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -5,7 +5,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     #default_product_id = fields.Many2one('product.template')
-    whatsapp_template_driver_confirm_id = fields.Many2one('mail.whatsapp.template')
-    whatsapp_template_driver_remember_cpe = fields.Many2one('mail.whatsapp.template')
-    whatsapp_template_customer_notify = fields.Many2one('mail.whatsapp.template')
+    whatsapp_template_driver_confirm_id = fields.Many2one('mail.whatsapp.template', config_parameter="trucking.wat_driver_confirm")
+    whatsapp_template_driver_remember_cpe = fields.Many2one('mail.whatsapp.template', config_parameter="trucking.wat_driver_remember_cpe")
+    whatsapp_template_customer_notify = fields.Many2one('mail.whatsapp.template', config_parameter="trucking.wat_customer_notify")
     
