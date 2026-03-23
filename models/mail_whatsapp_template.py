1437adee83e180e89bebdb59653898b530fb2ae9
Julio Santa Cruz <bartacruz@gmail.com>
2026-03-23 14:09:32 -0300

    [FIX] WhatsappTemplateButton.action_pressed

--- models/mail_whatsapp_template.py
+++ models/mail_whatsapp_template.py
@@ -19,16 +19,21 @@ class WhatsappTemplateButton(models.Model):
         confirm_template_id = self.env["ir.config_parameter"].sudo().get_param("trucking.wat_driver_confirm")
         if confirm_template_id  and self.template_id.id == int(confirm_template_id):
             trip = message.trucking_trip_id
-            _logger.warning("Button %s pressed for trip %s, author %s, message %s",self.name,trip.name,author.name,message)
+            _logger.info("Button %s pressed for trip %s, author %s, message %s",self.name,trip.name,author.name,message)
             if not trip or author.active_trucking_trip_id != trip:
-                return
-            if self.name =="Confirm":
+                _logger.warning("Button trip from message (%s) does not match author active trip (%s)",trip,author.active_trucking_trip_id)
+                body = 'El viaje sobre el que quieres actuar no está disponible.'
+            elif self.name =="Confirmar":
                 # Okay, this is a driver confiming a trip.
                 trip.driver_response = 'confirmed'
                 body = 'Confirmación recibida.\nNos estaremos contactando para mas detalles.'
-            else:
+            elif self.name =="Cancelar":
                 trip.driver_response = 'rejected'
                 body = 'Cancelación recibida.'
-            trip._send_whatsapp(self.author_id,body=body)
+            else:
+                _logger.warning("Unknown button name! %s",self.name)
+            if body:    
+                trip._send_whatsapp(author,body=body)
+            return
                 
         return super().action_pressed(author, message)
